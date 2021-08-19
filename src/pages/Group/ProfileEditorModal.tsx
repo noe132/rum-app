7a7217c97b4de4269f2d50cd4fbfa29feda41119
freedomsky <hanwei_1989@126.com>
2021-08-19 09:56:18 +0800

    tweak: hidden bind mixin logic for tmp

--- src/pages/Group/ProfileEditorModal.tsx
+++ src/pages/Group/ProfileEditorModal.tsx
@@ -6,7 +6,7 @@ import Dialog from 'components/Dialog';
 import Loading from 'components/Loading';
 import { TextField, Checkbox } from '@material-ui/core';
 import Button from 'components/Button';
-import MiddleTruncate from 'components/MiddleTruncate';
+// import MiddleTruncate from 'components/MiddleTruncate';
 import { sleep, isWindow } from 'utils';
 import { useStore } from 'store';
 import { client_id, getVerifierAndChanllege, getOAuthUrl } from 'utils/mixinOAuth';
@@ -283,24 +283,26 @@ const ProfileEditor = observer((props: IProps) => {
             margin="dense"
             variant="outlined"
           />
-          <div className="flex w-full px-12 mt-6">
-            <div className="p-2 pl-3 border border-black border-opacity-20 text-gray-500 text-12 truncate flex-1 rounded-l-4 border-r-0 hover:border-opacity-100">
-              <MiddleTruncate
-                string={state.profile.mixinUID || ''}
-                length={15}
-              />
-            </div>
-            <Button
-              noRound
-              className="rounded-r-4"
-              size="small"
-              onClick={() => {
-                state.openBindMixinModal = true;
-              }}
-            >
-              绑定 Mixin
-            </Button>
-          </div>
+          {
+            // <div className="flex w-full px-12 mt-6">
+            //  <div className="p-2 pl-3 border border-black border-opacity-20 text-gray-500 text-12 truncate flex-1 rounded-l-4 border-r-0 hover:border-opacity-100">
+            //    <MiddleTruncate
+            //      string={state.profile.mixinUID || ''}
+            //      length={15}
+            //    />
+            //  </div>
+            //  <Button
+            //    noRound
+            //    className="rounded-r-4"
+            //    size="small"
+            //    onClick={() => {
+            //      state.openBindMixinModal = true;
+            //    }}
+            //  >
+            //    绑定 Mixin
+            //  </Button>
+            // </div>
+          }
           <Tooltip
             enterDelay={600}
             enterNextDelay={600}
